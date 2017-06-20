30fbbf6d1e9f61d463a8474e58609129f1aa46b0
bwentzloff <brian.wentzloff@gmail.com>
2017-06-20 09:17:01 -0400

    oops. Add correct files

---
+++ part 3/botchart.py
@@ -0,0 +1,16 @@
+from poloniex import poloniex
+
+class BotChart(object):
+	def __init__(self, exchange, pair, period):
+		self.conn = poloniex('key goes here','key goes here')
+
+		self.pair = pair
+		self.period = period
+
+		self.startTime = 1491048000
+		self.endTime = 1491591200
+
+		self.data = self.conn.api_query("returnChartData",{"currencyPair":self.pair,"start":self.startTime,"end":self.endTime,"period":self.period})
+
+	def getPoints(self):
+		return self.data
---
+++ part 3/botindicators.py
@@ -0,0 +1,7 @@
+class BotIndicators(object):
+	def __init__(self):
+		 pass
+
+	def movingAverage(self, dataPoints, period):
+		if (len(dataPoints) > 1):
+			return sum(dataPoints[-period:]) / float(len(dataPoints[-period:]))
