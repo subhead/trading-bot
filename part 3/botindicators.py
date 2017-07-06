2e6d5e0f0985a800697127559a4d41c28e07bc7c
Anthony Wratten <awratten1@gmail.com>
2017-07-06 16:51:19 +1000

    Exponential Moving Average

--- part 3/botindicators.py
+++ part 3/botindicators.py
@@ -8,6 +8,19 @@ class BotIndicators(object):
 		if (len(dataPoints) > 1):
 			return sum(dataPoints[-period:]) / float(len(dataPoints[-period:]))
 		
+	def EMA(self, dataPoints, period, position=None, previous_ema=None):
+		"""https://www.oanda.com/forex-trading/learn/forex-indicators/exponential-moving-average"""
+		if (len(dataPoints) < period + 2):
+			return None
+		c = 2 / float(period + 1)
+		if not previous_ema:
+			return self.EMA(dataPoints, period, period, self.movingAverage(dataPoints[-period*2 + 1:-period + 1], period))
+		else:
+			current_ema = (c * dataPoints[-position]) + ((1 - c) * previous_ema)
+			if position > 0:
+				return self.EMA(dataPoints, period, position - 1, current_ema)
+		return previous_ema
+		
 	def momentum (self, dataPoints, period=14):
 		if (len(dataPoints) > period -1):
 			return dataPoints[-1] * 100 / dataPoints[-period]
